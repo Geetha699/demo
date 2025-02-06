4d8d8af3eadaf9674fbf4d2a22a64ae3f6177d0b
geetha699 <130438294+Geetha699@users.noreply.github.com>
2025-02-06 13:12:39 +0530

    creating infra

--- main.tf
+++ main.tf
@@ -203,8 +203,8 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = [aws_subnet.private_subnet.id, aws_subnet.private_subnet2.id]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 4
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
